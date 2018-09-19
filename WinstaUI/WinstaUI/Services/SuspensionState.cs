11ed29b8d4b77699898c1af2c5421b4ec13b034f
Mohsen <mohsens2@outlook.com>
2018-09-20 03:51:31 +0430

    init

---
+++ WinstaUI/WinstaUI/Services/SuspensionState.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WinstaUI.Services
+{
+    public class SuspensionState
+    {
+        public object Data { get; set; }
+
+        public DateTime SuspensionDate { get; set; }
+    }
+}
---
+++ WinstaUI/WinstaUI/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+using CommonServiceLocator;
+
+using GalaSoft.MvvmLight.Ioc;
+
+using WinstaUI.Services;
+using WinstaUI.Views;
+
+namespace WinstaUI.ViewModels
+{
+    [Windows.UI.Xaml.Data.Bindable]
+    public class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+            if (SimpleIoc.Default.IsRegistered<NavigationServiceEx>())
+            {
+                return;
+            }
+
+            SimpleIoc.Default.Register(() => new NavigationServiceEx());
+            SimpleIoc.Default.Register<ShellViewModel>();
+            Register<MainViewModel, MainPage>();
+            Register<SettingsViewModel, SettingsPage>();
+            Register<ShareTargetViewModel, ShareTargetPage>();
+            Register<SchemeActivationSampleViewModel, SchemeActivationSamplePage>();
+        }
+
+        public SchemeActivationSampleViewModel SchemeActivationSampleViewModel => ServiceLocator.Current.GetInstance<SchemeActivationSampleViewModel>();
+
+        public ShareTargetViewModel ShareTargetViewModel => ServiceLocator.Current.GetInstance<ShareTargetViewModel>();
+
+        public SettingsViewModel SettingsViewModel => ServiceLocator.Current.GetInstance<SettingsViewModel>();
+
+        public MainViewModel MainViewModel => ServiceLocator.Current.GetInstance<MainViewModel>();
+
+        public ShellViewModel ShellViewModel => ServiceLocator.Current.GetInstance<ShellViewModel>();
+
+        public NavigationServiceEx NavigationService => ServiceLocator.Current.GetInstance<NavigationServiceEx>();
+
+        public void Register<VM, V>()
+            where VM : class
+        {
+            SimpleIoc.Default.Register<VM>();
+
+            NavigationService.Configure(typeof(VM).FullName, typeof(V));
+        }
+    }
+}
